4e07ff3b2e6b903c29d1038c511e0b163cbbecd8
Nishkarsh Arora <78554387+JamesBond2002@users.noreply.github.com>
2022-06-25 10:38:40 +0530

    Updated segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,26 +1,25 @@
-class SegTree
-{
-    int n, VAL;
-    vt<int> tree;
+template <typename T>
+class SegTree {
+    int n;
+    T VAL;
+    vector<T> tree;
+
+    function<T(T, T)> todo;
 
     public:
-    SegTree(int _n, int val)
-    {
-        n = _n;
-        VAL = val;
+    SegTree(int n, int VAL, function<T(T, T)> todo) {
+        this->n = n;
+        this->VAL = VAL;
         tree.assign(2*n+2, VAL);
+        this->todo = todo;
     }
-    int todo(int a, int b) { return max(a, b); }
-    void update(int i, int x)
-    {
+    void update(int i, int x) {
         for(tree[i += n] = x; i > 1; i >>= 1)
             tree[i >> 1] = todo(tree[i], tree[i^1]);
     }
-    int get(int l, int r)
-    {
-        int res = VAL;
-        for(l += n, r += n+1; l<r; l >>= 1, r >>= 1)
-        {
+    T get(int l, int r) {
+        T res = VAL;
+        for(l += n, r += n+1; l<r; l >>= 1, r >>= 1) {
             if(l & 1) res = todo(res, tree[l++]);
             if(r & 1) res = todo(res, tree[--r]);
         }
